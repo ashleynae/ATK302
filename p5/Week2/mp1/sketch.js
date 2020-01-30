756ea0f27b8b869b16b42d058cb5f39ea54000d3
ashleynae <59887855+ashleynae@users.noreply.github.com>
2020-01-29 19:20:03 -0600

    avatar

--- p5/Week2/mp1/sketch.js
+++ p5/Week2/mp1/sketch.js
@@ -9,34 +9,35 @@ function draw() {
   background("blue") ;
   //stuff thst we always want to show
   fill(163, 107, 47) ;
-  rect(236,163,20,20)
+  rect(236,163,20,20) ;
   ellipse(246,127,75,75) ;
   //the head of the body
   fill("purple")
   rect(195,184,100,200,20) ;
-  fill(235, 64, 52)
+  fill(0)
   rect(210,384,20,100) ;
   rect(257,385,20,100) ;
-
+  fill(235, 64, 52)
+  quad(198,205,198,223,149,201,110,136) ;
+  quad(292,209,293,222,342,209,384,155) ;
+  //this is the eyes
 fill('brown') ;
-ellipse(228, 120, 50, 50);
-ellipse(264, 120, 50, 50);
+ellipse(228, 120, 40, 30) ;
+ellipse(264, 120, 40, 30) ;
 fill('white') ;
 ellipse(228, 120, 20, 20);
 ellipse(264, 120, 20, 20);
 
+
   if (mouseIsPressed) {
     //stuff that shows when the mouse is pressed
-    fill('brown') ;
+    fill('green') ;
     ellipse(228, 120, 50, 50);
     ellipse(264, 120, 50, 50);
 
   }
 
 
-  fill(0)
-  textSize(40) ;
-  text(mouseX + "," + mouseY, 250,250) ;
 }
 
 
